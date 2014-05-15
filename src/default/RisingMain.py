b4967672ab816810338e2301771fd0b9c563c11b
Rex Baumeister <rdbaumei@mtu.edu>
2014-05-15 04:03:16 -0400

    Tried (and failed) to make a background.

--- src/default/RisingMain.py
+++ src/default/RisingMain.py
@@ -14,13 +14,22 @@ windowWidth = 640
 windowHeight = 480
 windowSize = windowWidth, windowHeight
 
+mapWidth = windowWidth // 32
+mapHeight = windowHeight // 32
+
 speed = [1, 1] #Array/List declaration
 black = (0,0,0) #Tuple declaration
+
+print(mapWidth) 
+print(mapHeight)
 
 screen = pygame.display.set_mode(windowSize)
 
 axe = pygame.image.load("battle_axe2.png")
 axeRect = axe.get_rect()
+
+floor = pygame.image.load("rect_gray0.png")
+floorRect = floor.get_rect()
 
 while 1:
 	for event in pygame.event.get():
@@ -35,5 +44,11 @@ while 1:
 		
 	screen.fill(black)
 	screen.blit(axe, axeRect)
+	
+	for x in range(0, mapWidth):
+		for y in range(0, mapHeight):
+			floorRect.move_ip(x*32, y*32)
+			screen.blit(floor, floorRect)
+	
 	pygame.display.flip()
 
