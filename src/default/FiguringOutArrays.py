c58903947c51d5c65ec8e12acfce9676f789b15e
Rex Baumeister <rdbaumei@mtu.edu>
2014-06-05 21:16:43 -0400

    n sum stuff

--- src/default/FiguringOutArrays.py
+++ src/default/FiguringOutArrays.py
@@ -4,13 +4,29 @@ Created on Jun 3, 2014
 @author: Scurvy
 '''
 
-import sys
+import pygame, sys
+from pygame.locals import *
 
-flip = []
+tilesize = 32 #size of the square tile 
+windowWidth = 640
+windowHeight = 480
 
-for x in range(6):
-	flip.append([])
-	for y in range(4):
-		flip[x].append(x + y)
-		
-print(flip)
+speed = [2, 2]
+
+pygame.init()
+	
+#Create the window
+screen = pygame.display.set_mode((windowWidth, windowHeight))
+screen.fill((0,0,0))
+
+#Load the image
+tile = pygame.image.load("rect_gray0.png")
+rect = tile.get_rect()#(0, 0, tilesize, tilesize)
+sub = tile.subsurface(rect)
+image = pygame.Surface((tilesize, tilesize))
+
+rect = rect.move(speed)
+
+while pygame.event.wait().type != pygame.locals.QUIT:
+	image.blit(tile, rect) #Should be top-left corner?
+	pygame.display.flip()
--- src/default/RisingMain.py
+++ src/default/RisingMain.py
@@ -10,10 +10,11 @@ from pygame.locals import *
 
 windowWidth = 640
 windowHeight = 480
-windowSize = windowWidth, windowHeight
 
 mapWidth = windowWidth // 32
 mapHeight = windowHeight // 32
+
+tilesize = 32
 
 speed = [1, 1] #Array/List declaration
 black = (0,0,0) #Tuple declaration
@@ -21,8 +22,7 @@ black = (0,0,0) #Tuple declaration
 print(mapWidth) 
 print(mapHeight)
 
-def create_uniform_map():
-	
+def create_uniform_map():	
 	map = []
 	for x in range(mapHeight):
 		map.append([])
@@ -31,12 +31,25 @@ def create_uniform_map():
 			
 	return map 
 
+def draw_map(map):
+	
+	floor = pygame.image.load("rect_gray0.png")
+	rect = (0, 0, tilesize, tilesize)
+	sub = floor.subsurface(rect)
+	
+	image = pygame.Surface((tilesize, tilesize))
+	"""for x in range(mapHeight):
+		for y in range(mapWidth):
+			print(sub)"""
+	
+	image.blit(floor, (50,50))
+
 if __name__ == '__main__':
 
 	pygame.init()
 	print("Initializing")
-	screen = pygame.display.set_mode(windowSize)
-
+	
+	screen = pygame.display.set_mode((windowWidth, windowHeight))
 	screen.fill(black)
 
 	map = create_uniform_map()
@@ -46,6 +59,8 @@ if __name__ == '__main__':
 		for y in range(mapWidth):
 			print(map[x][y], end=" ")
 	
-
-
+	while pygame.event.wait().type != pygame.locals.QUIT:
+		draw_map(map)
+		pygame.display.flip()
+	
 
