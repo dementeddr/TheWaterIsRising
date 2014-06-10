9d50606846fec9da606065dd5bf872aa72174f9d
Rex Baumeister <rdbaumei@mtu.edu>
2014-06-10 00:59:31 -0400

    It still ain't workin'

--- src/default/FiguringOutArrays.py
+++ src/default/FiguringOutArrays.py
@@ -10,23 +10,37 @@ from pygame.locals import *
 tilesize = 32 #size of the square tile 
 windowWidth = 640
 windowHeight = 480
-
 speed = [2, 2]
+speed2 = [1,1]
 
 pygame.init()
 	
 #Create the window
 screen = pygame.display.set_mode((windowWidth, windowHeight))
-screen.fill((0,0,0))
 
 #Load the image
 tile = pygame.image.load("rect_gray0.png")
-rect = tile.get_rect()#(0, 0, tilesize, tilesize)
-sub = tile.subsurface(rect)
-image = pygame.Surface((tilesize, tilesize))
+tilerect = tile.get_rect()#(0, 0, tilesize, tilesize)
+tile2rect = tile.get_rect()
 
-rect = rect.move(speed)
-
-while pygame.event.wait().type != pygame.locals.QUIT:
-	image.blit(tile, rect) #Should be top-left corner?
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+			
+	tilerect = tilerect.move(speed)
+	if tilerect.left < 0 or tilerect.right > windowWidth:
+		speed[0] = -speed[0]
+	if tilerect.top < 0 or tilerect.bottom > windowHeight:
+		speed[1] = -speed[1]
+		
+	tile2rect = tile2rect.move(speed2)
+	if tile2rect.left < 0 or tile2rect.right > windowWidth:
+		speed2[0] = -speed2[0]
+	if tile2rect.top < 0 or tile2rect.bottom > windowHeight:
+		speed2[1] = -speed2[1]
+            
+	screen.fill((0, 0, 0))
+	screen.blit(tile, tilerect) #Should be top-left corner?
+	screen.blit(tile, tile2rect)
 	pygame.display.flip()
--- src/default/RisingMain.py
+++ src/default/RisingMain.py
@@ -22,45 +22,42 @@ black = (0,0,0) #Tuple declaration
 print(mapWidth) 
 print(mapHeight)
 
-def create_uniform_map():	
-	map = []
-	for x in range(mapHeight):
-		map.append([])
-		for y in range(mapWidth):
-			map[x].append(1)
-			
-	return map 
-
-def draw_map(map):
-	
+def create_map():
 	floor = pygame.image.load("rect_gray0.png")
-	rect = (0, 0, tilesize, tilesize)
-	sub = floor.subsurface(rect)
-	
-	image = pygame.Surface((tilesize, tilesize))
-	"""for x in range(mapHeight):
-		for y in range(mapWidth):
-			print(sub)"""
-	
-	image.blit(floor, (50,50))
+	map = []
+	for x in range(mapWidth):
+		line = []
+		map.append(line)
+		for y in range(mapHeight):
+			#tile = pygame.Rect(x*tilesize, y*tilesize, tilesize, tilesize)
+			line.append(floor.subsurface((0,0,tilesize,tilesize)))
+			
+	return map
+			
 
 if __name__ == '__main__':
 
 	pygame.init()
 	print("Initializing")
+	print('Path to pygame module:', pygame.__file__)
 	
 	screen = pygame.display.set_mode((windowWidth, windowHeight))
-	screen.fill(black)
+	map = create_map()
+	
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
 
-	map = create_uniform_map()
-	
-	for x in range(mapHeight):
-		print()
-		for y in range(mapWidth):
-			print(map[x][y], end=" ")
-	
-	while pygame.event.wait().type != pygame.locals.QUIT:
-		draw_map(map)
-		pygame.display.flip()
-	
+		screen.fill(black)
+						
+		for x in range(mapWidth):
+			for y in range(mapHeight):
+				tile = map[x][y]
+				#screen.blit(floor, tile)
+				screen.blit(tile, (x*tilesize, y*tilesize))
+				
+		screen.display.flip() 
+				
+
 
