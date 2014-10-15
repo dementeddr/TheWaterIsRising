6e9e1bcfc34e4ace2ee28b9c7bb5d54284a9d819
Rex Baumeister <rdbaumei@mtu.edu>
2014-10-15 13:48:31 -0400

    Random tiling. 9

--- src/default/TileMap1.py
+++ src/default/TileMap1.py
@@ -5,6 +5,7 @@ Created on Jun 11, 2014
 '''
 
 import os, sys
+from random import randint
 import pygame
 from pygame.locals import *
 
@@ -34,9 +35,13 @@ class TileMap1:
 			line = []
 			self.map.append(line)
 			for y in range(mapHeight):
-				if y == 0 or y == mapHeight-1 or x == 0 or x == mapWidth-1:
+				if (y == 0 or y == mapHeight-1 or x == 0 or x == mapWidth-1):
 					line.append([floor2.subsurface((0,0,tilesize,tilesize)), False])
 				else:
-					line.append([floor1.subsurface((0,0,tilesize,tilesize)), True])
+					rand = randint(0, 20)
+					if (rand == 1):
+						line.append([floor2.subsurface((0,0,tilesize,tilesize)), False])
+					else:
+						line.append([floor1.subsurface((0,0,tilesize,tilesize)), True])
 					
 		return
