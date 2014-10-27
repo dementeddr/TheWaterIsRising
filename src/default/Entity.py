81a7e81397d0b9b134515e442d7e32b191c6dcea
dementeddr <rdbaumei@mtu.edu>
2014-10-27 19:49:55 -0400

    Beginning the refactor. REVERT THIS ONE IF IT's HOPELESS

--- src/default/Entity.py
+++ src/default/Entity.py
@@ -3,3 +3,16 @@ Created on Oct 25, 2014
 
 @author: Rex Baumeister
 '''
+import os, sys, math
+import pygame
+from pygame.locals import *
+
+class Entitiy:
+	
+	
+	def __init__(self):
+		self.rect = pygame.Rect()
+		self.sprite = pygame.image()
+
+	def movement_update(self):
+			
